933f9bcbbff4d7d6e61113db11bf8b4c674cd73b
Roman Novakov <roman.novakov@playtech.com>
2023-07-22 22:24:26 +0300

    Changed MAX_HTTP = 4000

--- src/HAP.h
+++ src/HAP.h
@@ -73,8 +73,8 @@ struct HAPClient {
 
   // common structures and data shared across all HAP Clients
 
-  static const int MAX_HTTP=8095;                     // max number of bytes in HTTP message buffer
-  static const int MAX_CONTROLLERS=16;                // maximum number of paired controllers (HAP requires at least 16)
+  static const int MAX_HTTP=4000;                     // max number of bytes in HTTP message buffer
+  static const int MAX_CONTROLLERS=4;                // maximum number of paired controllers (HAP requires at least 16)
   static const int MAX_ACCESSORIES=41;                // maximum number of allowed Acessories (HAP limit=150, but not enough memory in ESP32 to run that many)
   
   static TLV<kTLVType,10> tlv8;                       // TLV8 structure (HAP Section 14.1) with space for 10 TLV records of type kTLVType (HAP Table 5-6)
